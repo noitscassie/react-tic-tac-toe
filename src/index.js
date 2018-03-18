e8334b8b0868ebee480e014ddf8310701d96e14f
Peter Johnstone <peter.wdj@gmail.com>
2018-03-18 07:38:15 +0000

    Moves state control from Square component to Board component

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -27,8 +27,19 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
